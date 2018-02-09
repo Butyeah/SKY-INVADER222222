529a928ee7174e3faaf2f313ee73b9d71c3ccd11
Tumeee <valkamo.tuomas@gmail.com>
2018-02-09 09:40:04 +0200

    0.23
    Kameran liikkeelle lähtö on sulavaa. Muokkasin MainMenuButtons koodia.

---
+++ Sky Invader/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+	void Start () {
+		Time.timeScale = 0.00000000001f;
+	}
+
+	public void PlayGame() {
+		SceneManager.LoadScene ("lv01");
+	}
+
+	public void QuitGame () {
+		Application.Quit ();
+	}
+}
