d01c0063c26ecf08c21064e893bf0a115035f9e3
Nokkanen Teemu <s1500533@student.businesscollege.fi>
2018-02-16 10:13:56 +0200

    Counter and coins
    Adds score counter and coins

---
+++ Sky Invader/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score;
+
+	// Muuttuja edustaa ScoreCounter-tekstilaatikkoa
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+
+		text = GetComponent<Text>();
+
+		score = 0;		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Jos score menee alle nollan, niin estetään se
+		if (score < 0)
+			score = 0;
+		// Päivittää ScoreCounteria
+		text.text = "" + score;
+
+	}
+
+	public static void AddPoints(int pointsToAdd)
+	{
+		score += pointsToAdd;
+	}
+}
